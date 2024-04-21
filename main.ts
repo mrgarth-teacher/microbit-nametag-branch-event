650bc1ddd6d6cdaea55a6d046b01b83cc87c632e
mrgarth-teacher <110578347+mrgarth-teacher@users.noreply.github.com>
2024-04-21 11:51:31 +1000

    Add clear screens to ensure cleaner output

--- main.ts
+++ main.ts
@@ -1,5 +1,7 @@
 input.onButtonPressed(Button.A, function () {
     basic.showString("Mr G")
+    basic.clearScreen()
     basic.showIcon(IconNames.Heart)
+    basic.clearScreen()
     basic.showString("Digitech")
 })
